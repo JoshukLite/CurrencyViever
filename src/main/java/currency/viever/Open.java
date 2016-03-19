5da32f542b8274d330ea7acec1699b93e96e34d4
Vitaliy <katkovvitaliy@gmail.com>
2016-03-19 23:08:05 +0200

    Update Open.java

--- src/main/java/currency/viever/Open.java
+++ src/main/java/currency/viever/Open.java
@@ -1,7 +1,13 @@
 package currency.viever;
+import javax.swing.SwingUtilities;
 
 public class Open {
 	public static void main(String[] args) {
-		new EXwindow();
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new EXwindow();
+			}
+		});
 	}
 }
